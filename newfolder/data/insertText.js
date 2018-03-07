bbdc795d7ec2432f5503327ed61cba4c17fafa23
ryvo <ryvo@centrum.cz>
2018-03-07 12:47:10 +0100

    Update insertText.js

--- newfolder/data/insertText.js
+++ newfolder/data/insertText.js
@@ -1,5 +1,6 @@
 function insertText(elemID, text)
 {
+    // Protoze netestujes na undefined je zbytecne vytvaret promennou. Dej to vse na jeden radek.
     var elem = document.getElementById(elemID);
     elem.innerHTML = text;
 }
