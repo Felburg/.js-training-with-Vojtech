ff8d763d6dec8fdfc4e63f808b5942cc229c15c8
Felburg <32649315+Felburg@users.noreply.github.com>
2018-03-18 20:00:45 +0100

    Create insertText.js

---
+++ PROJECT_001_OBJECT_BASIC/data/insertText.js
@@ -0,0 +1,10 @@
+function insertText (text){
+    var elem = document.getElementById('textarea1');
+    elem.innerHTML = text;
+}
+
+var texty = {
+    insertText1: 'Napsano111',
+    insertText2: 'Napsano222',
+    insertText3: 'Napsano333'
+};
